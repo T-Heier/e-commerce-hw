222befea370567ca055d22341f9e3dac6bde16f1
T Heier <trentheier@gmail.com>
2021-05-02 16:07:28 -0400

    added a couple getroutes, my data-seed does not seem to be working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,15 +1,39 @@
 const router = require('express').Router();
+const { reset } = require('nodemon');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try {
+    const catData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  try {
+    const catData = await Driver.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    
+    if (!catData) {
+      res.status(404).json({ message: `No category found with that id!` });
+      return;
+    }
+    
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+
   // be sure to include its associated Products
 });
 
